9ba7a9e079efa50323188b04e96549df394d0886
disman <disman810@gmail.com>
2020-04-02 18:21:14 +0700

    Menambahkan modul artikel

---
+++ app/Artikel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artikel extends Model
+{
+
+    protected $table = "table_artikel";
+    protected $guarded = ['id'];
+
+    public function kategori()
+    {
+        return $this->belongsTo(\App\Kategori::class, 'categoris_id', 'id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'title';
+    }
+}
